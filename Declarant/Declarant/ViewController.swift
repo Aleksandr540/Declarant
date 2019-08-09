65c9b0755b07a94a99df49783b8b3eea5ada3cf5
Aleksandr Kryukov <kryukov86@mail.ru>
2019-08-09 14:19:40 +0700

    add initial commit

---
+++ Declarant/Declarant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Declarant
+//
+//  Created by Александр Крюков on 09/08/2019.
+//  Copyright © 2019 Александр Крюков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
